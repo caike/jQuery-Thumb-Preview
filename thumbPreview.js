bfd48cc4baf9096da8c81fbafa6b5d9730f130a5
Carlos Souza <carloshrsouza@gmail.com>
2013-11-01 09:33:48 -0400

    Initial code

---
+++ thumbPreview.js
@@ -0,0 +1,54 @@
+/**
+ *  jQuery Thumb Preview Plugin
+ *
+ *  (c) 2013 Carlos Souza
+ *  MIT licensed
+ *
+ *  Requires jQuery, not included in this file.
+ *
+ *
+ *  Example:
+ *
+ *   Setting up the thumbnails:
+ *
+ *   <img src="/path/to/default_image.jpg"
+ *      data-images='["/path/to/thumb_1.jpg", "/path/to/thumb_2.jpg", "/path/to/thumb_3.jpg"]' />
+ *
+ *   Activating preview:
+ *
+ *   $('.list img').thumbPreview();
+ *
+ */
+(function($){
+
+  var changeImage = function(img, allImages, positionX, sectionWidth){
+    var imagesCount = allImages.length;
+    var setDefault = true;
+
+    for(var i=imagesCount-1; i>0; i--){
+      if(positionX > (sectionWidth*i)){
+        img.attr('src', allImages[i]);
+        setDefault = false;
+        break;
+      }
+    }
+
+    if(setDefault){
+      img.attr('src', allImages[0]);
+    }
+  }
+
+  $.fn.thumbPreview = function(){
+
+    this.on('mousemove', function(e){
+      var target = $(e.target);
+      var allImages = target.data('images');
+      var sectionWidth = parseInt(target.css('width')) / allImages.length;
+      var positionX = e.clientX - $(this).offset().left;
+      changeImage(target, allImages, positionX, sectionWidth)
+    });
+
+    return this;
+  }
+}(jQuery));
+
